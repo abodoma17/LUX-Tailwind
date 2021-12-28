a1e5d0a34c3acd405febb26bc7a1d4741bd785aa
abodoma17 <o.a.ahmed3@student.aast.edu>
2021-12-29 00:55:24 +0200

    Added first and last name validation

--- public/Pages/Users/View/validation.js
+++ public/Pages/Users/View/validation.js
@@ -1,6 +1,8 @@
 var input = 1;
 const phoneInputField = document.getElementById('phoneNum');
 const emailInputField = document.getElementById('emailInput');
+const firstNameInputField = document.getElementById('firstNameInput');
+const lastNameInputField = document.getElementById('lastNameInput');
 const button = document.getElementById('checkoutButton');
 
 phoneInputField.addEventListener('focusout', () => {
@@ -48,3 +50,59 @@ emailInputField.addEventListener('focusout', () => {
         document.getElementById("emailError").classList.add('hidden');
     }
 })
+
+firstNameInputField.addEventListener('focusout', () => {
+    var firstName = document.getElementById('firstNameInput').value;
+    console.log(firstName)
+    if(stringContainsNumber(firstName))
+    {
+        button.disabled = true;
+        console.log("includes num")
+        document.getElementById('checkoutButton').classList.add('cursor-not-allowed');
+        document.getElementById('firstNameInput').classList.add('border-2');
+        document.getElementById('firstNameInput').classList.add('border-red-600');
+        document.getElementById("firstNameError").classList.remove('hidden');
+    }
+    else
+    {
+        button.disabled = false;
+        console.log("doesnt Include num")
+        document.getElementById('checkoutButton').classList.remove('cursor-not-allowed');
+        document.getElementById('firstNameInput').classList.remove('border-2');
+        document.getElementById('firstNameInput').classList.remove('border-red-600');
+        document.getElementById("firstNameError").classList.add('hidden');
+    }
+})
+
+lastNameInputField.addEventListener('focusout', () => {
+    var firstName = document.getElementById('lastNameInput').value;
+    console.log(firstName)
+    if(stringContainsNumber(firstName))
+    {
+        button.disabled = true;
+        console.log("includes num")
+        document.getElementById('checkoutButton').classList.add('cursor-not-allowed');
+        document.getElementById('lastNameInput').classList.add('border-2');
+        document.getElementById('lastNameInput').classList.add('border-red-600');
+        document.getElementById("lastNameError").classList.remove('hidden');
+    }
+    else
+    {
+        button.disabled = false;
+        console.log("doesnt Include num")
+        document.getElementById('checkoutButton').classList.remove('cursor-not-allowed');
+        document.getElementById('lastNameInput').classList.remove('border-2');
+        document.getElementById('lastNameInput').classList.remove('border-red-600');
+        document.getElementById("lastNameError").classList.add('hidden');
+    }
+})
+
+function stringContainsNumber(name){
+    let nameC = String(name);
+    for( let i = 0; i < nameC.length; i++){
+        if(!isNaN(nameC.charAt(i)) && !(nameC.charAt(i) === " ") ){
+          return true;
+        }
+    }
+    return false;
+  }
