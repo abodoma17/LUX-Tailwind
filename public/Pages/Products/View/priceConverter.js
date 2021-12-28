1434100d48f42d0669947b1dcca5edc40525eb1a
abodoma17 <o.a.ahmed3@student.aast.edu>
2021-12-28 20:43:59 +0200

    Price converter js function

---
+++ public/Pages/Products/View/priceConverter.js
@@ -0,0 +1,16 @@
+
+
+function convert()
+{
+    var price = document.getElementById('price'), currency=document.getElementById('currencySelect');
+    console.log(price.innerHTML);
+    console.log(currency.value);
+    if(currency.value == "usd")
+    {
+        price.innerHTML = price.innerHTML/16;
+    }
+    else if(currency.value == "egp")
+    {
+        price.innerHTML = (price.innerHTML*16).toPrecision(2);
+    }
+}
