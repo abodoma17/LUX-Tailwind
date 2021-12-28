094928bf456f36789bb22b0bc1724770f473a633
abodoma17 <o.a.ahmed3@student.aast.edu>
2021-12-28 21:16:14 +0200

    Added email validation

--- public/Pages/Users/View/validation.js
+++ public/Pages/Users/View/validation.js
@@ -1,8 +1,9 @@
 var input = 1;
-const inputField = document.getElementById('phoneNum');
+const phoneInputField = document.getElementById('phoneNum');
+const emailInputField = document.getElementById('emailInput');
 const button = document.getElementById('checkoutButton');
 
-inputField.addEventListener('focusout', () => {
+phoneInputField.addEventListener('focusout', () => {
     var input = document.getElementById('phoneNum').value.length;
     console.log(input)
     if(input < 11 || input > 11)
@@ -22,5 +23,28 @@ inputField.addEventListener('focusout', () => {
         document.getElementById("phoneError").classList.add('hidden');
         button.disabled = false;
         console.log(input.length);
+    }
+})
+
+emailInputField.addEventListener('focusout', () => {
+    var email = document.getElementById('emailInput').value;
+    console.log(email)
+    if(!(email.includes("@")))
+    {
+        button.disabled = true;
+        console.log("doesnt include @")
+        document.getElementById('checkoutButton').classList.add('cursor-not-allowed');
+        document.getElementById('emailInput').classList.add('border-2');
+        document.getElementById('emailInput').classList.add('border-red-600');
+        document.getElementById("emailError").classList.remove('hidden');
+    }
+    else
+    {
+        button.disabled = false;
+        console.log("includes @")
+        document.getElementById('checkoutButton').classList.remove('cursor-not-allowed');
+        document.getElementById('emailInput').classList.remove('border-2');
+        document.getElementById('emailInput').classList.remove('border-red-600');
+        document.getElementById("emailError").classList.add('hidden');
     }
 })
