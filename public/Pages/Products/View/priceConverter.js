5038d60095945b3b79ade065e7636254df84e4ad
abodoma17 <o.a.ahmed3@student.aast.edu>
2022-01-05 01:52:55 +0200

    Added API calling to get today's exchange rate from EGP to USD

--- public/Pages/Products/View/priceConverter.js
+++ public/Pages/Products/View/priceConverter.js
@@ -2,12 +2,20 @@ const originalPrice = document.getElementById('price').innerHTML;
 
 function convert()
 {
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/egp/usd.json',false);
+    Httpreq.send(null);
+    var jsonC = Httpreq.responseText;
+    jsonC = JSON.parse(jsonC);
+    var exchangeRate = jsonC.usd;
+
     var price = document.getElementById('price'), currency=document.getElementById('currencySelect');
     console.log(price.innerHTML);
     console.log(currency.value);
     if(currency.value == "usd")
     {
-        price.innerHTML = price.innerHTML/16;
+        var usdPrice = price.innerHTML*exchangeRate;
+        price.innerHTML = usdPrice.toPrecision(3);
     }
     else if(currency.value == "egp")
     {
