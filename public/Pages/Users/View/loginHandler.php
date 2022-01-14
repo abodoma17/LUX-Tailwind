d19f455f6af1419c18776e9d99865ad00ca8a0d4
Youssef Meleka <youssefatef00@gmail.com>
2022-01-14 20:52:19 +0200

    injection on both pass and username for admin corrected

--- public/Pages/Users/View/loginHandler.php
+++ public/Pages/Users/View/loginHandler.php
@@ -1,11 +1,10 @@
 <?php
 
-    $username= $_POST["username"];
-    $password = mysqli_real_escape_string($con,$_POST["password"]); //sql injection protected
-   
-
     $con = mysqli_connect("localhost","root","") or die ("Error: Couldn't connect to srever");
     $db = mysqli_select_db($con,"luxdb") or die ("Error: Couldn't connect to Database");
+
+    $username= mysqli_real_escape_string($con,$_POST["username"]); //sql injection protected
+    $password = mysqli_real_escape_string($con,$_POST["password"]); //sql injection protected
 
     $login = "SELECT username,password FROM users WHERE username='$username' and password='$password' ";
 
