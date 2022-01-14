b60429afeb3e30d7fb6acf92aa3e029722278449
abodoma17 <o.a.ahmed3@student.aast.edu>
2022-01-14 17:41:17 +0200

    Linked product page to DB and made it fully functional.

--- public/Pages/Products/View/hoverPic.js
+++ public/Pages/Products/View/hoverPic.js
@@ -1,9 +1,15 @@
-const pic = document.getElementById('productImg');
-const originalSrc = document.getElementById('productImg').src;
+const image1 = document.getElementById('productImg1');
+const image2 = document.getElementById('productImg2');
 
 function changePic(){
-    pic.src = "../../../Shared/Images/statue1.jpg";
-}
-function restorePic(){
-    pic.src = originalSrc;
+    if(image1.classList.contains('hidden') && !(image2.classList.contains('hidden')))
+    {
+        image1.classList.remove('hidden');
+        image2.classList.add('hidden');
+    }
+    else
+    {
+        image1.classList.add('hidden');
+        image2.classList.remove('hidden');
+    }
 }
--- public/Pages/Products/View/product.php
+++ public/Pages/Products/View/product.php
@@ -51,45 +51,60 @@
             </div>
         </div>
 
+        <?php
+        $id = $_GET['id'];
         
-        <div class="md:grid md:grid-cols-3">
+        $con = mysqli_connect("localhost","root","") or die ("Error: Couldn't connect to srever");
+                    $db = mysqli_select_db($con,"luxdb");
 
-            <div class="w-11/12 mx-auto my-10">
-                <img id="productImg" src="../../../Shared/Images/accessories.jpg" onmouseover="changePic();" onmouseout="restorePic();">
-            </div>
+                    $viewProduct = "SELECT * FROM products
+                     WHERE id = $id";
+                    $result = mysqli_query($con, $viewProduct);
 
-            <div class="md:col-span-2 my-12 ml-5">
+                    while($row = mysqli_fetch_array($result))
+                    {
+                        echo " 
+                        <div class='md:grid md:grid-cols-3'>
+                            <div class='w-11/12 mx-auto my-10'>
+                                <img class='' id='productImg1' src='".$row["image1"]."' onmouseover='changePic();' onmouseout='changePic();'>
+                                <img class='hidden' id='productImg2' src='".$row["image2"]."' onmouseover='changePic();' onmouseout='changePic();'>
+                            </div>
 
-                <h1 class="text-5xl mb-1 font-serif">Sphinx Necklace Pendant</h1>
-                <h2 class="text-2xl font-mono" id="">Unisex necklace pendant made of stainless steel. </h2>
-                <div class="flex">
-                    <p class="text-xl text-yellow-100 font-nunito mr-2" id="price">50.00</p>
+                            <div class='md:col-span-2 my-12 ml-5'>
+                                <h1 class='text-5xl mb-1 font-serif'>" . $row["name"] . "</h1>
+                                <h2 class='text-2xl font-mono' id=''>" . $row["description"] . "</h2>
+                                <div class='flex'>
+                                    <p class='text-xl text-yellow-100 font-nunito mr-2' id='price'>" . $row["price"] . "</p>
+                                    <select class='text-white bg-primary border-2 border-gray-500' id='currencySelect' onchange='convert();'>
+                                        <option value='egp'>EGP</option>
+                                        <option value='usd'>US$</option>
+                                    </select>
+                                </div>
 
-                    <select class="text-white bg-primary border-2 border-gray-500" id="currencySelect" onchange="convert();">
-                        <option value="egp">EGP</option>
-                        <option value="usd">US$</option>
-                    </select>
-                    
-                </div>
+                                <button type='button' class='my-4 font-' style='background-color: #a48111;
+                                color: rgb(255, 255, 255);
+                                text-transform: uppercase;
+                                padding: 20px;
+                                border-radius: 10px;
+                                border: none;
+                                border-bottom: 6px solid #0002 ;
+                                font-weight: 700;'>Add to cart</button>  
 
-                <button type="button" class="my-4 font-" style="background-color: #a48111;
-                color: rgb(255, 255, 255);
-                text-transform: uppercase;
-                padding: 20px;
-                border-radius: 10px;
-                border: none;
-                border-bottom: 6px solid #0002 ;
-                font-weight: 700;">Add to cart</button>  
-                
-                <h5 class="text-l font-semibold mb-3 font-nunito">The original sphinx had a long braided beard and a nose, and it was painted in bright colours. </h5>
+                                <h5 class='text-l font-semibold mb-3 font-nunito'>" . $row["bg"] . "</h5>
+                                <h3 class='text-2xl border-b-2 inline font-serif' id=''>Dimensions:</h3>
 
-                <h3 class="text-2xl border-b-2 inline font-serif" id="">Dimensions:</h3>
-                    
-                <h4 class="text-l mt-5 font-bold">Weight: 350gm</h4>
-                <h4 class="text-l font-bold">Height: 5cm</h4>
-                <h4 class="text-l font-bold">Width: 2cm</h4>
-            </div>
-        </div>
+                                <h4 class='text-l mt-5 font-bold'>Weight: " . $row["weight"] . "gm</h4>
+                                <h4 class='text-l font-bold'>Height: " . $row["height"] . "cm</h4>
+                                <h4 class='text-l font-bold'>Width: " . $row["width"] . "cm</h4>
+                            </div>
+                        </div>
+                        ";
+                    }
+
+        ?>
+
+        
+        
     
 
         <footer class="w-full bg-black mt-10 md:mt-32">
