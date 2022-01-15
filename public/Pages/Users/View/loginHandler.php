d78ba5d5dc04ae7ad002392ee41f9c72bdcffffe
abodoma17 <o.a.ahmed3@student.aast.edu>
2022-01-15 20:43:37 +0200

    Added hashing to passwords in db and added SQL Injection protection

--- public/Pages/Users/View/loginHandler.php
+++ public/Pages/Users/View/loginHandler.php
@@ -1,25 +1,26 @@
 <?php
 
-    $username= $_POST["username"];
-    $password = $_POST["password"];
-
     $con = mysqli_connect("localhost","root","") or die ("Error: Couldn't connect to srever");
     $db = mysqli_select_db($con,"luxdb") or die ("Error: Couldn't connect to Database");
 
-    $login = "SELECT username,password FROM users WHERE username='$username' and password='$password' ";
+    $username= mysqli_real_escape_string($con,$_POST["username"]);
+    $password = mysqli_real_escape_string($con,$_POST["password"]);
+
+    $login = "SELECT * FROM users";
 
     $result = mysqli_query($con,$login);
     $count = mysqli_num_rows($result);
-
-    if($count==1){
-
+    $row = mysqli_fetch_array($result);
+    $username1 = $row["username"];
+    $password1 = $row["password"];
+    if($username == $username1 && password_verify($password, $password1))
+    {
         session_start();
         $_SESSION["loggedAdmin"]=$username;
         header('Location: adminFunc.php');
-
-
-    }else {
-
+    }
+    else
+    {
         header('Location: admin.php');
     }
 ?>
